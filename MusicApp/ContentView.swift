484e6ab461ec50d9d6d4bacd39da03c641b477b7
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 15:17:42 -0400

    create album & song structs with properties

--- MusicApp/ContentView.swift
+++ MusicApp/ContentView.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+struct Album : Hashable {
+    var id = UUID()
+    var name : String
+    var image : String
+    var songs : [Song]
+}
+
+struct Song : Hashable {
+    var id = UUID()
+    var name : String
+    var time : String
+     
+}
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
