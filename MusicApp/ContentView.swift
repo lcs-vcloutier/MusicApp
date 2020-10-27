36ad5bcc9084c772a9bce31296be21b9ac37702e
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 18:18:37 -0400

    added arrays for albums and song really just dummy content will be fixing this later

--- MusicApp/ContentView.swift
+++ MusicApp/ContentView.swift
@@ -21,8 +21,32 @@ struct Song : Hashable {
     
 }
 struct ContentView: View {
-    var albums = [Album(name: "Album 1", image: <#T##String#>, songs: <#T##[Song]#>)]
-
+    var albums = [Album(name: "Album 1", image: "1",
+                        songs: [Song(name: "1", time: "2:36"),
+                                Song(name: "2", time: "2:36"),
+                                Song(name: "3", time: "2:36"),
+                                Song(name: "4", time: "2:36")]),
+                  Album(name: "Album 2", image: "2",
+                        songs: [Song(name: "1", time: "2:36"),
+                                Song(name: "2", time: "2:36"),
+                                Song(name: "3", time: "2:36"),
+                                Song(name: "4", time: "2:36")]),
+                  Album(name: "Album 3", image: "3",
+                        songs: [Song(name: "1", time: "2:36"),
+                                Song(name: "2", time: "2:36"),
+                                Song(name: "3", time: "2:36"),
+                                Song(name: "4", time: "2:36")]),
+                  Album(name: "Album 4", image: "4",
+                        songs: [Song(name: "1", time: "2:36"),
+                                Song(name: "2", time: "2:36"),
+                                Song(name: "3", time: "2:36"),
+                                Song(name: "4", time: "2:36")]),
+                  Album(name: "Album 5", image: "5",
+                        songs: [Song(name: "1", time: "2:36"),
+                                Song(name: "2", time: "2:36"),
+                                Song(name: "3", time: "2:36"),
+                                Song(name: "4", time: "2:36")])]
+    
     var body: some View {
         NavigationView {
             ScrollView {
