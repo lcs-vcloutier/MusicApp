4d124168d3c4a7ae90ad0cad4edc66b4d54ac858
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 19:28:14 -0400

    player view done, need to connect to firebase

--- MusicApp/PlayerView.swift
+++ MusicApp/PlayerView.swift
@@ -27,17 +27,17 @@ struct PlayerView:  View {
                     Color.white.cornerRadius(20).shadow(radius: 10)
                     
                     HStack{
-                        Button(action: self.playPause, label: {
+                        Button(action: self.previous, label: {
                             Image(systemName: "arrow.left.circle").resizable()
-                        }).frame(width: 70, height: 70, alignment: .center).foregroundColor(Color.black.opacity(0.2))
+                        }).frame(width: 50, height: 50, alignment: .center).foregroundColor(Color.black.opacity(0.2))
                         
                         Button(action: self.playPause, label: {
                             Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill").resizable()
-                        }).frame(width: 70, height: 70, alignment: .center)
+                        }).frame(width: 70, height: 70, alignment: .center).padding()
                          
-                        Button(action: self.playPause, label: {
+                        Button(action: self.next, label: {
                             Image(systemName: "arrow.right.circle").resizable()
-                        }).frame(width: 70, height: 70, alignment: .center).foregroundColor(Color.black.opacity(0.2))
+                        }).frame(width: 50, height: 50, alignment: .center).foregroundColor(Color.black.opacity(0.2))
                     }
                 }.edgesIgnoringSafeArea(.bottom).frame(height: 200, alignment: .center)
             }
