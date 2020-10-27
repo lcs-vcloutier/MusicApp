edd262d3cf2075b2ba97cd7fc0252b1b5ad3d3cb
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 18:59:04 -0400

    created player view file

---
+++ MusicApp/PlayerView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerView.swift
+//  MusicApp
+//
+//  Created by Cloutier, Vincent on 2020-10-27.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlayerView:  View {
+    var body: some View {
+        Text("test")
+    }
+}
