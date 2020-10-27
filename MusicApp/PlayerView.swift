f70a93c01c676b7da5439525931b133ff42dff4b
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 19:26:40 -0400

    player view tweaked

--- MusicApp/PlayerView.swift
+++ MusicApp/PlayerView.swift
@@ -21,7 +21,7 @@ struct PlayerView:  View {
             VStack{
                 Spacer()
                 AlbumArt(album: album, isWithText: false)
-                Text(song.name)
+                Text(song.name).font(.title).fontWeight(.light).foregroundColor(.white)
                 Spacer()
                 ZStack{
                     Color.white.cornerRadius(20).shadow(radius: 10)
@@ -39,7 +39,7 @@ struct PlayerView:  View {
                             Image(systemName: "arrow.right.circle").resizable()
                         }).frame(width: 70, height: 70, alignment: .center).foregroundColor(Color.black.opacity(0.2))
                     }
-                }
+                }.edgesIgnoringSafeArea(.bottom).frame(height: 200, alignment: .center)
             }
         }
     }
