40da5a1ac7829957e38bd557947a32593fada439
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 15:14:43 -0400

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Cloutier, Vincent on 2020-10-27.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
